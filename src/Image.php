d356201294d565c65a87ff0b1908ecf6b003aad3
freek <freek@spatie.be>
2017-01-31 23:46:37 +0100

    fix save method

--- src/Image.php
+++ src/Image.php
@@ -33,10 +33,7 @@ class Image
         $this->imageDriver = $imageDriver;
     }
 
-    /**
-     * @param callable|Manipulations $manipulations
-     */
-    public function manipulate($manipulations): self
+    public function manipulate($manipulations)
     {
         if (is_callable($manipulations)) {
             $manipulations($this->manipulations);
@@ -62,7 +59,7 @@ class Image
 
     public function save($outputPath = '')
     {
-        if ($path = '') {
+        if ($outputPath == '') {
             $outputPath = $this->pathToImage;
         }
 
