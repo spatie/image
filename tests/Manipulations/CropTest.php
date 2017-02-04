278fa21e4d2d316fc5469b2ff9da4bfe31aa5c39
freek <freek@spatie.be>
2017-02-04 18:55:16 +0100

    commit

--- tests/Manipulations/CropTest.php
+++ tests/Manipulations/CropTest.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\Image\Test\Manipulations;
 
+use Spatie\Image\Exceptions\InvalidManipulation;
 use Spatie\Image\Image;
 use Spatie\Image\Manipulations;
 use Spatie\Image\Test\TestCase;
@@ -16,6 +17,30 @@ class CropTest extends TestCase
         Image::load($this->getTestJpg())->crop(Manipulations::CROP_BOTTOM, 100, 500)->save($targetFile);
 
         $this->assertFileExists($targetFile);
+    }
+
+    /** @test */
+    public function it_will_throw_an_exception_when_passing_an_invalid_crop_method()
+    {
+        $this->expectException(InvalidManipulation::class);
+
+        Image::load($this->getTestJpg())->crop('blabla', 10, 10);
+    }
+
+    /** @test */
+    public function it_will_throw_an_exception_when_passing_a_negative_width()
+    {
+        $this->expectException(InvalidManipulation::class);
+
+        Image::load($this->getTestJpg())->crop(Manipulations::CROP_BOTTOM, -10, 10);
+    }
+
+    /** @test */
+    public function it_will_throw_an_exception_when_passing_a_negative_height()
+    {
+        $this->expectException(InvalidManipulation::class);
+
+        Image::load($this->getTestJpg())->crop(Manipulations::CROP_BOTTOM, 10, -10);
     }
 
 
