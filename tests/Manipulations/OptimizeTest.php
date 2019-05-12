4e3044e5456d47f6b065df6dd6c48669db5f9682
Nicolas Lemoine <nico.lemoine@gmail.com>
2019-05-12 15:42:58 +0200

    Fixes `optimize()` when used with `apply()` (#78) * Don't return after first group loop * Add tests

--- tests/Manipulations/OptimizeTest.php
+++ tests/Manipulations/OptimizeTest.php
@@ -21,6 +21,19 @@ class OptimizeTest extends TestCase
     }
 
     /** @test */
+    public function it_can_optimize_an_image_when_using_apply()
+    {
+        $targetFile = $this->tempDir->path('optimized.jpg');
+
+        Image::load($this->getTestFile('test.jpg'))
+            ->apply()
+            ->optimize()
+            ->save($targetFile);
+
+        $this->assertFileExists($targetFile);
+    }
+
+    /** @test */
     public function it_can_optimize_an_image_with_the_given_optimization_options()
     {
         $targetFile = $this->tempDir->path('optimized.jpg');
