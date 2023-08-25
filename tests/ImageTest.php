e4eaf98af9291e927f51fe38d2612bae8c81a126
freekmurze <freekmurze@users.noreply.github.com>
2023-08-25 16:16:03 +0000

    Fix styling

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -4,14 +4,14 @@ use Spatie\Image\Drivers\ImageDriver;
 use Spatie\Image\Exceptions\CouldNotLoadImage;
 use Spatie\Image\Image;
 
-it('can load an image', function() {
+it('can load an image', function () {
     $image = Image::load(getTestJpg());
 
     expect($image)->toBeInstanceOf(ImageDriver::class);
 });
 
-it('will throw an exception when no file exists at the given path', function() {
-    $invalidPath = getTestJpg() . 'non-existing';
+it('will throw an exception when no file exists at the given path', function () {
+    $invalidPath = getTestJpg().'non-existing';
 
     Image::load($invalidPath);
 })->throws(CouldNotLoadImage::class);
