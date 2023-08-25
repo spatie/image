4b23ce2e18b0f3c68398f618039d2577b6ad3d11
Tim Van Dijck <tim.van.dijck@gmail.com>
2023-08-25 17:34:01 +0200

    Add docs.

--- src/Drivers/ImageDriver.php
+++ src/Drivers/ImageDriver.php
@@ -2,6 +2,8 @@
 
 namespace Spatie\Image\Drivers;
 
+use Spatie\Image\Exceptions\InvalidManipulation;
+
 interface ImageDriver
 {
     public function load(string $path): self;
@@ -12,5 +14,10 @@ interface ImageDriver
 
     public function getHeight(): int;
 
+    /**
+     * @param int $brightness A value between -100 and 100
+     *
+     * @throws InvalidManipulation
+     */
     public function brightness(int $brightness): self;
 }
