cfae03703f7a2f2c0f1e2fff5aa4847a4c2d419a
Freek Van der Herten <freek@spatie.be>
2023-08-28 17:51:12 +0200

    Update fit dimensions in FitTest.php

--- tests/Manipulations/FitTest.php
+++ tests/Manipulations/FitTest.php
@@ -8,7 +8,9 @@ use Spatie\Image\Enums\Fit;
 it('can fit an image in the given dimensions', function (ImageDriver $driver) {
     $targetFile = $this->tempDir->path("{$driver->driverName()}/fit.jpg");
 
-    $driver->load(getTestJpg())->fit(Fit::Contain, 500, 300)->save($targetFile);
+    $driver->load(getTestJpg())->fit(Fit::Contain, 100, 60)->save($targetFile);
 
     expect($targetFile)->toBeFile();
+
+    // TODO: add assertions around the dimensions of the image
 })->with('drivers');
