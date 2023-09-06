b6b394ad3b2819271fa77a735982af6065acb6b3
Freek Van der Herten <freek@spatie.be>
2023-09-06 12:17:14 +0200

    add crop test

--- tests/Manipulations/CropTest.php
+++ tests/Manipulations/CropTest.php
@@ -1,18 +1,24 @@
 <?php
 
+use Spatie\Image\Drivers\ImageDriver;
 use Spatie\Image\Enums\CropPosition;
 use Spatie\Image\Image;
 
-it('can crop an image relative to a position', function() {
-    $driver = Image::useImageDriver('gd');
-
+it('can crop an image relative to a position', function(
+    ImageDriver $driver,
+    array $cropArguments,
+    int $expectedWidth,
+    int $expectedHeight,
+) {
     $targetFile = $this->tempDir->path("{$driver->driverName()}/manual-crop.jpg");
 
-    $driver->load(getTestJpg())->crop(500, 100, CropPosition::TopLeft)->save($targetFile);
+    $driver->load(getTestJpg())->crop(... $cropArguments)->save($targetFile);
 
     expect($targetFile)->toBeFile();
 
     $savedImage = $driver->load($targetFile);
-    expect($savedImage->getWidth())->toBe(50);
-    expect($savedImage->getHeight())->toBe(50);
-});
+    expect($savedImage->getWidth())->toBe($expectedWidth);
+    expect($savedImage->getHeight())->toBe($expectedHeight);
+})->with('drivers')->with([
+    [[50, 100, CropPosition::TopLeft], 50, 100],
+]);
