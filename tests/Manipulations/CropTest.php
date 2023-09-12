1657da03193774364e1d138f87f7e9fdc0a1775d
Freek Van der Herten <freek@spatie.be>
2023-09-12 11:10:54 +0200

    add test

--- tests/Manipulations/CropTest.php
+++ tests/Manipulations/CropTest.php
@@ -27,5 +27,6 @@ it('can crop an image relative to a position', function (
     [[50, 100, CropPosition::Top], 50, 100],
     [[50, 100, CropPosition::Left], 50, 100],
     [[50, 100, CropPosition::Right], 50, 100],
+    [[50, 100, CropPosition::BottomRight], 50, 100],
 
 ]);
