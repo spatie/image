c8bb51514da8181ad444913db2d01cfe25bbc1df
Freek Van der Herten <freek@spatie.be>
2023-09-12 17:57:06 +0200

    Refactor file paths for color tests

--- tests/Base64Test.php
+++ tests/Base64Test.php
@@ -3,7 +3,6 @@
 use Spatie\Image\Drivers\ImageDriver;
 
 it('can get a base64 string for an image', function (ImageDriver $driver) {
-
     $base64 = $driver->load(getTestFile('transparent-bg.png'))->base64();
 
     expect($base64)->toBeString();
