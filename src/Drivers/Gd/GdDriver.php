aef655bc157c4eedd613a40327cd35029e7bf4f3
freekmurze <freekmurze@users.noreply.github.com>
2023-09-12 12:02:37 +0000

    Fix styling

--- src/Drivers/Gd/GdDriver.php
+++ src/Drivers/Gd/GdDriver.php
@@ -540,8 +540,7 @@ class GdDriver implements ImageDriver
         AlignPosition $position = AlignPosition::Center,
         int $x = 0,
         int $y = 0,
-    ): self
-    {
+    ): self {
         if (is_string($otherImage)) {
             $otherImage = (new self())->load($otherImage);
         }
