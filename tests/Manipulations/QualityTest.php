0501f9e8aa30848a2091281bda4c4f277ad672b2
Freek Van der Herten <freek@spatie.be>
2023-09-25 12:18:47 +0200

    v3

--- tests/Manipulations/QualityTest.php
+++ tests/Manipulations/QualityTest.php
@@ -4,18 +4,18 @@ use Spatie\Image\Drivers\ImageDriver;
 
 use function Spatie\Snapshots\assertMatchesImageSnapshot;
 
-it('can set the quality of a png', function (ImageDriver $driver) {
+it('can set the quality of a png', function (ImageDriver $driver, int $quality) {
     $targetFile = $this->tempDir->path("{$driver->driverName()}/quality.png");
 
-    $driver->load(getTestJpg())->quality(20)->save($targetFile);
+    $driver->load(getTestJpg())->quality($quality)->save($targetFile);
 
     assertMatchesImageSnapshot($targetFile);
-})->with('drivers');
+})->with('drivers')->with([10, 50, 90]);
 
-it('can set the quality for different formats', function (ImageDriver $driver, string $format) {
+it('can set the quality for different formats', function (ImageDriver $driver, string $format, int $quality) {
     $targetFile = $this->tempDir->path("{$driver->driverName()}/quality.{$format}");
 
-    $driver->load(getTestJpg())->quality(20)->save($targetFile);
+    $driver->load(getTestJpg())->quality($quality)->save($targetFile);
 
     expect($targetFile)->toBeFile();
-})->with('drivers', ['jpg', 'gif', 'webp', 'png']);
+})->with('drivers')->with(['jpg', 'png', 'webp'])->with([10, 50, 90]);
