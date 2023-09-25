4af0d7fb7b86a0e7c35fcd6ea87c8c1f80d07ff1
Freek Van der Herten <freek@spatie.be>
2023-09-25 12:27:32 +0200

    improve test

--- tests/Manipulations/QualityTest.php
+++ tests/Manipulations/QualityTest.php
@@ -4,13 +4,20 @@ use Spatie\Image\Drivers\ImageDriver;
 
 use function Spatie\Snapshots\assertMatchesImageSnapshot;
 
-it('can set the quality of a png', function (ImageDriver $driver, int $quality) {
-    $targetFile = $this->tempDir->path("{$driver->driverName()}/quality.png");
+it('can set the quality of a png', function (ImageDriver $driver) {
+    $lowQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality10.png");
+    $driver->load(getTestJpg())->quality(10)->save($lowQualityTargetFile);
 
-    $driver->load(getTestJpg())->quality($quality)->save($targetFile);
+    $mediumQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality50.png");
+    $driver->load(getTestJpg())->quality(50)->save($mediumQualityTargetFile);
 
-    assertMatchesImageSnapshot($targetFile);
-})->with('drivers')->with([10, 50, 90]);
+    $highQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality90.png");
+    $driver->load(getTestJpg())->quality(90)->save($highQualityTargetFile);
+
+    expect(filesize($lowQualityTargetFile))->toBeLessThan(filesize($mediumQualityTargetFile));
+
+    expect(filesize($mediumQualityTargetFile))->toBeLessThan(filesize($highQualityTargetFile));
+})->with('drivers');
 
 it('can set the quality for different formats', function (ImageDriver $driver, string $format, int $quality) {
     $targetFile = $this->tempDir->path("{$driver->driverName()}/quality.{$format}");
