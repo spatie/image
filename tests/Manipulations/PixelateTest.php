7096b7bcfc6d7979122377381d93a682f26b958a
Freek Van der Herten <freek@spatie.be>
2023-09-29 11:33:05 +0200

    fix method name

--- tests/Manipulations/PixelateTest.php
+++ tests/Manipulations/PixelateTest.php
@@ -4,7 +4,7 @@ use Spatie\Image\Drivers\ImageDriver;
 
 use function Spatie\Snapshots\assertMatchesImageSnapshot;
 
-it('can blur an image', function (ImageDriver $driver) {
+it('can pixelate an image', function (ImageDriver $driver) {
     $targetFile = $this->tempDir->path("{$driver->driverName()}/pixelate.png");
 
     $driver->load(getTestJpg())->pixelate()->save($targetFile);
