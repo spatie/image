0d992a78bc56dae7a9b2db0cf09d330a496e1f0c
Tim Van Dijck <tim.van.dijck@gmail.com>
2023-10-11 17:06:00 +0200

    Simplify tests.

--- tests/ImageFormatTest.php
+++ tests/ImageFormatTest.php
@@ -4,37 +4,13 @@ use Spatie\Image\Drivers\ImageDriver;
 use Spatie\Image\Exceptions\UnsupportedImageFormat;
 use Spatie\Image\Image;
 
-it('can save a jpeg', function (ImageDriver $driver) {
-    $targetFile = $this->tempDir->path("{$driver->driverName()}/format-test.jpeg");
+it('can save supported formats', function (ImageDriver $driver, string $format) {
+    $targetFile = $this->tempDir->path("{$driver->driverName()}/format-test." . $format);
 
     $driver->load(getTestJpg())->save($targetFile);
 
-    expect($targetFile)->toHaveMime('image/jpeg');
-})->with('drivers');
-
-it('can save a png', function (ImageDriver $driver) {
-    $targetFile = $this->tempDir->path("{$driver->driverName()}/format-test.png");
-
-    $driver->load(getTestJpg())->save($targetFile);
-
-    expect($targetFile)->toHaveMime('image/png');
-})->with('drivers');
-
-it('can save a webp', function (ImageDriver $driver) {
-    $targetFile = $this->tempDir->path("{$driver->driverName()}/format-test.webp");
-
-    $driver->load(getTestJpg())->save($targetFile);
-
-    expect($targetFile)->toHaveMime('image/webp');
-})->with('drivers');
-
-it('can save a gif', function (ImageDriver $driver) {
-    $targetFile = $this->tempDir->path("{$driver->driverName()}/format-test.gif");
-
-    $driver->load(getTestJpg())->save($targetFile);
-
-    expect($targetFile)->toHaveMime('image/gif');
-})->with('drivers');
+    expect($targetFile)->toHaveMime('image/' . $format);
+})->with('drivers', ['jpeg', 'gif', 'png', 'webp']);
 
 it('can save a heic with Imagick', function () {
     $driver = Image::useImageDriver('imagick');
