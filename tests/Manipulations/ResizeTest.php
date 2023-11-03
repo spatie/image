ad5653b386511e61967ffe3686d37f265788b0f1
Tim Van Dijck <tim.van.dijck@gmail.com>
2023-11-03 10:43:30 +0100

    Resize test.

--- tests/Manipulations/ResizeTest.php
+++ tests/Manipulations/ResizeTest.php
@@ -4,10 +4,17 @@ use Spatie\Image\Drivers\ImageDriver;
 
 use function Spatie\Snapshots\assertMatchesImageSnapshot;
 
-it('can colorize an image', function (ImageDriver $driver) {
+it('can resize an image', function (ImageDriver $driver) {
     $targetFile = $this->tempDir->path("{$driver->driverName()}/resize.png");
 
-    $driver->load(getTestJpg())->resize(100, 70)->save($targetFile);
+    $image = $driver->load(getTestJpg())->resize(100, 70)->save($targetFile);
+
+    ray($image->getWidth(), $image->getHeight());
+
+    expect($image->getWidth())->toBe(100);
+    expect($image->getHeight())->toBe(70);
 
     assertMatchesImageSnapshot($targetFile);
+
+    $this->markTestIncomplete('TODO: results of resize on GD look like a crop, not a resize.');
 })->with('drivers');
