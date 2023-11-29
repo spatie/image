6a81da21ee27507658ffba79d88d591989d44ce6
Freek Van der Herten <freek@spatie.be>
2023-11-29 08:35:22 +0100

    make save path optional

--- src/Drivers/ImageDriver.php
+++ src/Drivers/ImageDriver.php
@@ -19,7 +19,7 @@ interface ImageDriver
 
     public function load(string $path): self;
 
-    public function save(string $path): self;
+    public function save(string $path = ''): self;
 
     public function getWidth(): int;
 
