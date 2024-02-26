c17f3c65a93c90e20fe9e9071dc0c1e6dc0d3114
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-26 12:02:41 +0100

    Don't support quality on avif for now.

--- tests/Manipulations/QualityTest.php
+++ tests/Manipulations/QualityTest.php
@@ -2,7 +2,7 @@
 
 use Spatie\Image\Drivers\ImageDriver;
 
-it('can set the quality of an image jpg', function (ImageDriver $driver, string $format) {
+it('can set the quality of an image', function (ImageDriver $driver, string $format) {
     if ($format === 'avif' && ! function_exists('imageavif')) {
         $this->markTestSkipped('avif is not supported on this system');
 
@@ -21,67 +21,4 @@ it('can set the quality of an image jpg', function (ImageDriver $driver, string 
     expect(filesize($lowQualityTargetFile))->toBeLessThan(filesize($mediumQualityTargetFile));
 
     expect(filesize($mediumQualityTargetFile))->toBeLessThan(filesize($highQualityTargetFile));
-})->with('drivers')->with(['jpg']);
-
-it('can set the quality of an image png', function (ImageDriver $driver, string $format) {
-    if ($format === 'avif' && ! function_exists('imageavif')) {
-        $this->markTestSkipped('avif is not supported on this system');
-
-        return;
-    }
-
-    $lowQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality10.{$format}");
-    $driver->loadFile(getTestJpg())->quality(10)->save($lowQualityTargetFile);
-
-    $mediumQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality50.{$format}");
-    $driver->loadFile(getTestJpg())->quality(50)->save($mediumQualityTargetFile);
-
-    $highQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality90.{$format}");
-    $driver->loadFile(getTestJpg())->quality(90)->save($highQualityTargetFile);
-
-    expect(filesize($lowQualityTargetFile))->toBeLessThan(filesize($mediumQualityTargetFile));
-
-    expect(filesize($mediumQualityTargetFile))->toBeLessThan(filesize($highQualityTargetFile));
-})->with('drivers')->with(['png']);
-
-it('can set the quality of an image webp', function (ImageDriver $driver, string $format) {
-    if ($format === 'avif' && ! function_exists('imageavif')) {
-        $this->markTestSkipped('avif is not supported on this system');
-
-        return;
-    }
-
-    $lowQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality10.{$format}");
-    $driver->loadFile(getTestJpg())->quality(10)->save($lowQualityTargetFile);
-
-    $mediumQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality50.{$format}");
-    $driver->loadFile(getTestJpg())->quality(50)->save($mediumQualityTargetFile);
-
-    $highQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality90.{$format}");
-    $driver->loadFile(getTestJpg())->quality(90)->save($highQualityTargetFile);
-
-    expect(filesize($lowQualityTargetFile))->toBeLessThan(filesize($mediumQualityTargetFile));
-
-    expect(filesize($mediumQualityTargetFile))->toBeLessThan(filesize($highQualityTargetFile));
-})->with('drivers')->with(['webp']);
-
-it('can set the quality of an image avif', function (ImageDriver $driver, string $format) {
-    if ($format === 'avif' && ! function_exists('imageavif')) {
-        $this->markTestSkipped('avif is not supported on this system');
-
-        return;
-    }
-
-    $lowQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality10.{$format}");
-    $driver->loadFile(getTestJpg())->quality(10)->save($lowQualityTargetFile);
-
-    $mediumQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality50.{$format}");
-    $driver->loadFile(getTestJpg())->quality(50)->save($mediumQualityTargetFile);
-
-    $highQualityTargetFile = $this->tempDir->path("{$driver->driverName()}/quality90.{$format}");
-    $driver->loadFile(getTestJpg())->quality(90)->save($highQualityTargetFile);
-
-    expect(filesize($lowQualityTargetFile))->toBeLessThan(filesize($mediumQualityTargetFile));
-
-    expect(filesize($mediumQualityTargetFile))->toBeLessThan(filesize($highQualityTargetFile));
-})->with('drivers')->with(['avif']);
+})->with('drivers')->with(['jpg', 'png', 'webp']);
