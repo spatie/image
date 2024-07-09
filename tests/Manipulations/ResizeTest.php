60e8b7c739c412c0522703c06f54a9a41eb96ba9
olexoliinyk0 <147385264+olexoliinyk0@users.noreply.github.com>
2024-07-09 23:31:58 +0300

    test: GD resizeCanvas for pngs with transparent bg;

--- tests/Manipulations/ResizeTest.php
+++ tests/Manipulations/ResizeTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Spatie\Image\Drivers\ImageDriver;
+use Spatie\Image\Image;
 
 use function Spatie\Snapshots\assertMatchesImageSnapshot;
 
@@ -14,3 +15,18 @@ it('can resize an image', function (ImageDriver $driver) {
 
     assertMatchesImageSnapshot($targetFile);
 })->with('drivers');
+
+it('can resize canvas of transparent pngs without loosing transparency when GD is used', function () {
+    $image = Image::useImageDriver(ImageDriver::Gd)->loadFile(getTestFile('transparent.png')); // 1640x923
+    $targetFile = $this->tempDir->path("{$driver->driverName()}/resize-with-transparent-png.png");
+    $transparent = imagecolorallocatealpha($canvas->image, 255, 255, 255, 127);
+    expect($image->getHeight())->toEqual(923);
+    expect($image->pickColor(0, 0, \Spatie\Image\Enums\ColorFormat::Int))->toEqual($transparent);
+
+    // make it square, so height is added top and bottom, bg transparent
+    $image->resizeCanvas(1640, 1640, \Spatie\Image\Enums\AlignPosition::Center, false, $image->pickColor(0, 0, \Spatie\Image\Enums\ColorFormat::Rgba))->save($targetFile);
+
+    $targetImage = $driver->loadFile($targetFile);
+    expect($targetImage->getHeight())->toEqual(1640);
+    expect($targetImage->pickColor(0, 0, \Spatie\Image\Enums\ColorFormat::Int))->toEqual($transparent);
+});
