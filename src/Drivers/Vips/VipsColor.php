5f71b51b2097b9312ab5c575c866e4b497e70cba
riasvdv <3626559+riasvdv@users.noreply.github.com>
2025-01-17 13:12:44 +0000

    Fix styling

--- src/Drivers/Vips/VipsColor.php
+++ src/Drivers/Vips/VipsColor.php
@@ -2,12 +2,6 @@
 
 namespace Spatie\Image\Drivers\Vips;
 
-use ImagickPixel;
-use Spatie\Image\Drivers\Color;
 use Spatie\Image\Drivers\Gd\GdColor;
-use Spatie\Image\Exceptions\InvalidColor;
 
-class VipsColor extends GdColor
-{
-
-}
+class VipsColor extends GdColor {}
