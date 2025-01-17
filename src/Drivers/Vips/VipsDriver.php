d8ed086da58a47d218b10eafef5a4a549eeed412
riasvdv <3626559+riasvdv@users.noreply.github.com>
2025-01-17 13:34:09 +0000

    Fix styling

--- src/Drivers/Vips/VipsDriver.php
+++ src/Drivers/Vips/VipsDriver.php
@@ -145,7 +145,7 @@ class VipsDriver implements ImageDriver
         $sepia = Image::newFromArray([
             [0.393, 0.769, 0.189],
             [0.349, 0.686, 0.168],
-            [0.272, 0.534, 0.131]
+            [0.272, 0.534, 0.131],
         ]);
 
         if ($this->image->hasAlpha()) {
