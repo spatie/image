e3ad74ea6bf4bd2124d29eac7b5aee5f36cd060c
Pepijn Peeters <me@pepijnpeeters.nl>
2025-03-27 13:29:24 +0100

    Add custom driver test

--- tests/Pest.php
+++ tests/Pest.php
@@ -60,6 +60,13 @@ dataset('drivers', [
     'gd' => [Image::useImageDriver('gd')],
 ]);
 
+class CustomDriver extends \Spatie\Image\Drivers\Imagick\ImagickDriver {
+    public function driverName(): string
+    {
+        return 'custom';
+    }
+}
+
 expect()->extend('toHaveMime', function (string $expectedMime) {
     $file = finfo_open(FILEINFO_MIME_TYPE);
     $actualMime = finfo_file($file, $this->value);
